2ec235c6671519b03a4146b8d34113155406bc09
Poololol <braedonegrillot@gmail.com>
2025-01-19 23:22:32 -0600

    Add Number of cards drawing & Change XOfAKind Detection

--- deck.py
+++ deck.py
@@ -4,6 +4,9 @@ import pygame
 import utils
 import random
 
+font = pygame.font.Font(None, 36)
+random.seed(1)
+
 class Deck(CardGroup):
     def __init__(self, cards: list[Card] | None = None):
         '''
@@ -13,18 +16,32 @@ class Deck(CardGroup):
         if cards is None:
             cards = [Card(value, suit) for value in range(1, 14) for suit in range(4)]
         super().__init__(cards)
+        self.originalCards = self.cards.copy()
         random.shuffle(self.cards)
 
-    def draw(self, surface: pygame.Surface, pos: tuple[float, float], size: tuple[float, float]) -> None:
+    def draw(self, surface: pygame.Surface, pos: tuple[float, float], size: tuple[float, float], num: bool = False) -> None:
         '''
         Draws the deck of cards
         '''
         super().draw(surface, pos, size, style='Stack', direction='Up', face='Down', spacing=.33)
+        if num:
+            self.drawNumCards(surface, (pos[0] + size[0]/2, pos[1] + size[1]), size)
 
-    def deal(self, num: int) -> list[Card]:
+    def drawNumCards(self, surface: pygame.Surface, pos: tuple[float, float], size: tuple[float, float]) -> None:
+        '''
+        Draws the number of cards in the deck
+        Pos is the top-center of the text
+        '''
+        text = font.render(f'{len(self.cards)}/{len(self.originalCards)}', True, utils.black)
+        surface.blit(text, (pos[0] - text.get_width()/2, pos[1]))
+        
+
+    def deal(self, num: int, hand: CardGroup | None = None) -> list[Card]:
         '''
         Deals a number of cards from the deck
         '''
+        if hand is not None:
+            hand.addCards(self.cards[-num:])
         return [self.cards.pop() for _ in range(num)]
     
     def shuffle(self) -> None:
